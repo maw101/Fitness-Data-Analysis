c187c9818dcd7b97ee05498b883483b1fa411b7a
maw101 <maw101@aber.ac.uk>
2019-08-29 13:27:29 +0100

    Point to data points

--- cycling/red_route_cycle_analysis.py
+++ cycling/red_route_cycle_analysis.py
@@ -2,3 +2,9 @@ import gpxpy
 
 gpx_file = open('data/red_route_cycle.gpx', 'r')
 gpx = gpxpy.parse(gpx_file)
+
+# point to data points
+data = gpx.tracks[0].segments[0].points
+
+start_position = data[0]
+end_position = data[-1]
