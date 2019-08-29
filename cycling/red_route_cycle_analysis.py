3c1af468a3473a0834a8c55509f4a0cf1cc4c0a1
maw101 <maw101@aber.ac.uk>
2019-08-29 13:28:04 +0100

    Setup pandas dataframe

--- cycling/red_route_cycle_analysis.py
+++ cycling/red_route_cycle_analysis.py
@@ -1,4 +1,5 @@
 import gpxpy
+import pandas as pd
 
 gpx_file = open('data/red_route_cycle.gpx', 'r')
 gpx = gpxpy.parse(gpx_file)
@@ -8,3 +9,13 @@ data = gpx.tracks[0].segments[0].points
 
 start_position = data[0]
 end_position = data[-1]
+
+# setup dataframe
+df = pd.DataFrame(columns=['lon', 'lat', 'ele', 'time'])  # create empty dataframe
+for point in data:  # iterate over all data points adding them to the dataframe
+    df = df.append({'lon': point.longitude, 'lat': point.latitude, 'ele': point.elevation, 'time': point.time},
+                   ignore_index=True)
+
+df.index = df.time
+
+print(df.head())  # print head of dataframe for debug
